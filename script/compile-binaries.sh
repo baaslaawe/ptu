d6fa5e05af1fd78b787a9b60701259946af33531
Ivan Ilves <ivan.ilves@redbooth.com>
2015-08-20 23:58:30 +0200

    Cross-compile script and first binary
    Former-commit-id: 514da448364f69b5d5c71a56419db07375b87143

---
+++ script/compile-binaries.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NAME=ptu
+
+for GOOS in darwin linux windows; do
+  for GOARCH in 386 amd64; do
+    echo "* Building $GOOS-$GOARCH"
+    export GOOS=$GOOS
+    export GOARCH=$GOARCH
+    go build -o bin/${NAME}-$GOOS-$GOARCH
+  done
+done
